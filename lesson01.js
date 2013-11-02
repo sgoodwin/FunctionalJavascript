03ebdbe373bce2d464d298db6e750ecfb3715388
Samuel Goodwin <samuel@roundwallsoftware.com>
2013-11-02 01:07:40 +0100

    Fixed errors.

--- lesson01.js
+++ lesson01.js
@@ -1,3 +1,3 @@
 module.exports =function uppercase(string){
   return string.toUpperCase();
-}
+};
--- lesson07.js
+++ lesson07.js
@@ -1,5 +1,5 @@
 module.exports = function reduce(array, func, initial){
-  ar value = func.call(this, initial, array[0]);
+  var value = func.call(this, initial, array[0]);
 
   if(array.length > 1){
     return reduce(array.slice(1), func, value);
